cdf4566506554bc32810f0ad09889eb6d244fdb8
CASL0 <gin.0046.822@gmail.com>
2023-08-16 22:59:22 +0900

    preferenceのデータ層の実装

---
+++ lib/core/data/source/local/local_settings_data_source.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+
+import 'package:errorlookup/core/data/source/settings_data_source.dart';
+import 'package:errorlookup/core/models/result.dart';
+import 'package:errorlookup/core/models/theme_data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/// データ層の実装
+class LocalSettingsDataSource implements SettingsDataSource {
+  /// テーマ設定のプリファレンスマップ
+  static const _themeValue = {
+    ThemeMode.system: "theme_system",
+    ThemeMode.dark: "theme_dark",
+    ThemeMode.light: "theme_light"
+  };
+
+  /// テーマ設定のインバースマップ
+  static final _themeValueInverse =
+      _themeValue.map((key, value) => MapEntry(value, key));
+
+  static const _themeKey = "theme";
+
+  /// テーマ設定のストリーム
+  final _themeSettingStreamController = StreamController<ThemeMode>();
+
+  LocalSettingsDataSource({bool initialize = true}) {
+    if (initialize) {
+      init();
+    }
+  }
+
+  Future<void> init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedValue = prefs.getString(_themeKey);
+    if (savedValue == null) {
+      prefs.setString(_themeKey, "theme_system");
+      _themeSettingStreamController.sink
+          .add(_themeValueInverse["theme_system"]!);
+    } else {
+      _themeSettingStreamController.sink.add(_themeValueInverse[savedValue]!);
+    }
+  }
+
+  @override
+  Stream getThemeSettingStream() {
+    return _themeSettingStreamController.stream;
+  }
+
+  @override
+  Future<Result<bool, Exception>> saveThemeSetting(
+      {required final ThemeMode newMode}) async {
+    final prefs = await SharedPreferences.getInstance();
+    final convertedValue = _themeValue[newMode];
+    if (convertedValue == null) {
+      return Failure(Exception("preference value is not found"));
+    }
+    final ret = await prefs.setString(_themeKey, convertedValue);
+    if (!ret) {
+      return Failure(Exception("SharedPreferences.setString fail"));
+    }
+    _themeSettingStreamController.sink.add(newMode);
+    return const Success(true);
+  }
+}
---
+++ test/core/data/source/local/local_settings_data_source_test.dart
@@ -0,0 +1,29 @@
+import 'package:errorlookup/core/data/source/local/local_settings_data_source.dart';
+import 'package:errorlookup/core/models/result.dart';
+import 'package:errorlookup/core/models/theme_data.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  group("LocalSettingsDataSourceTest", () {
+    setUp(() async {
+      SharedPreferences.setMockInitialValues({});
+    });
+    test("テーマ設定がプリファレンスにない場合はシステムを初期値として保存する", () async {
+      final dataSource = LocalSettingsDataSource(initialize: false);
+      await dataSource.init();
+      final prefs = await SharedPreferences.getInstance();
+      expect(prefs.getString("theme"), "theme_system");
+    });
+    test("設定変更後ストリームに流れてくる", () async {
+      final dataSource = LocalSettingsDataSource();
+      final verifyStream = expectLater(
+          dataSource.getThemeSettingStream(), emitsInOrder([ThemeMode.dark]));
+      final ret = await dataSource.saveThemeSetting(newMode: ThemeMode.dark);
+      expect(ret is Success, true);
+      await verifyStream;
+      final prefs = await SharedPreferences.getInstance();
+      expect(prefs.getString("theme"), "theme_dark");
+    });
+  });
+}
