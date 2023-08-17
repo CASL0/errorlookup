dabc73c7133094701b70436e9179f211ce5a2e68
CASL0 <gin.0046.822@gmail.com>
2023-08-17 23:34:05 +0900

    設定画面のUI状態を定義

---
+++ lib/feature/settings/models/settings_state.dart
@@ -0,0 +1,11 @@
+import 'package:errorlookup/core/models/theme_data.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'settings_state.freezed.dart';
+
+/// 設定画面のUI状態
+@freezed
+class SettingsState with _$SettingsState {
+  const factory SettingsState({@Default(ThemeMode.system) ThemeMode theme}) =
+      _SettingsState;
+}
