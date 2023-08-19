f07e3563581ca7af9ae797a6329225ed769f1936
CASL0 <gin.0046.822@gmail.com>
2023-08-19 14:07:45 +0900

    テーマ設定ダイアログからpreferenceの設定値を変更できるようにした

--- lib/feature/settings/pages/settings_page.dart
+++ lib/feature/settings/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:errorlookup/core/common/views/top_app_bar.dart';
+import 'package:errorlookup/feature/settings/viewmodels/settings_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,13 +9,17 @@ class SettingsPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(settingsViewModelProvider.notifier);
+
     return Scaffold(
         appBar: TopAppBar(
           currentRouteName: "settings",
         ),
         body: ListView(
           children: [
-            ListTile(title: const Text("Theme"), onTap: () => {}),
+            ListTile(
+                title: const Text("Theme"),
+                onTap: () => viewModel.openThemeSetting(context)),
           ],
         ));
   }
--- lib/feature/settings/viewmodels/settings_view_model.dart
+++ lib/feature/settings/viewmodels/settings_view_model.dart
@@ -1,7 +1,11 @@
+import 'package:errorlookup/core/data/repository/default_settings_repository.dart';
 import 'package:errorlookup/core/data/repository/settings_repository.dart';
+import 'package:errorlookup/core/data/source/local/local_settings_data_source.dart';
 import 'package:errorlookup/core/models/result.dart';
-import 'package:errorlookup/core/models/theme_data.dart';
+import 'package:errorlookup/core/models/theme_data.dart' as theme_data;
 import 'package:errorlookup/feature/settings/models/settings_state.dart';
+import 'package:errorlookup/feature/settings/views/theme_setting_dialog.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// 設定画面のビジネスロジックを扱うVieModel
@@ -10,7 +14,7 @@ class SettingsViewModel extends StateNotifier<SettingsState> {
       : _settingsRepository = settingsRepository,
         super(const SettingsState()) {
     _settingsRepository.getThemeSettingStream().listen((event) {
-      if (event is ThemeMode) {
+      if (event is theme_data.ThemeMode) {
         state = state.copyWith(theme: event);
       }
     });
@@ -20,7 +24,24 @@ class SettingsViewModel extends StateNotifier<SettingsState> {
 
   /// テーマ設定を保存します
   Future<Result<bool, Exception>> saveThemeSetting(
-      {required final ThemeMode newMode}) async {
+      {required final theme_data.ThemeMode newMode}) async {
     return await _settingsRepository.saveThemeSetting(newMode: newMode);
   }
+
+  /// テーマ設定画面を開きます
+  void openThemeSetting(final BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => ThemeSettingDialog(
+            selectedTheme: state.theme,
+            themeChanged: (e) {
+              saveThemeSetting(newMode: e);
+            }));
+  }
 }
+
+final settingsViewModelProvider =
+    StateNotifierProvider<SettingsViewModel, SettingsState>((ref) =>
+        SettingsViewModel(
+            settingsRepository: DefaultSettingsRepository(
+                dataSource: LocalSettingsDataSource())));
