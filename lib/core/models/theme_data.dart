f4c2836d28dbee5b7f84656dac23f5953461cec1
CASL0 <gin.0046.822@gmail.com>
2023-08-19 14:00:47 +0900

    テーマのモード表示名を定義

--- lib/core/models/theme_data.dart
+++ lib/core/models/theme_data.dart
@@ -1,2 +1,9 @@
 /// テーマのモード
 enum ThemeMode { system, light, dark }
+
+/// テーマのモードの表示名
+final themeDisplayName = {
+  ThemeMode.system: "System",
+  ThemeMode.light: "Light",
+  ThemeMode.dark: "Dark"
+};
