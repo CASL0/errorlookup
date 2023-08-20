c20a9a4fefa446a23074fa45d6029f7f1cdd5a60
CASL0 <gin.0046.822@gmail.com>
2023-08-20 14:27:07 +0900

    テーマ設定変更後にテーマが反映されるようにした

--- lib/core/models/theme_data.dart
+++ lib/core/models/theme_data.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart' as material;
+
 /// テーマのモード
 enum ThemeMode { system, light, dark }
 
@@ -6,4 +8,10 @@ final themeDisplayName = {
   ThemeMode.system: "System",
   ThemeMode.light: "Light",
   ThemeMode.dark: "Dark"
+};
+
+final convertTheme = {
+  ThemeMode.system: material.ThemeMode.system,
+  ThemeMode.light: material.ThemeMode.light,
+  ThemeMode.dark: material.ThemeMode.dark
 };
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,50 @@
+import 'package:errorlookup/core/data/di/data_modules.dart';
+import 'package:errorlookup/core/data/repository/settings_repository.dart';
+import 'package:errorlookup/core/models/main_state.dart';
+import 'package:errorlookup/core/models/theme_data.dart' as theme_data;
 import 'package:errorlookup/core/router/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  setupDataModules();
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mainState = ref.watch(_mainViewModelProvider);
+
     return MaterialApp.router(
       title: "errorlookup",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       darkTheme: ThemeData.dark(),
+      themeMode: theme_data.convertTheme[mainState.theme]!,
       routerConfig: router,
     );
   }
 }
+
+class _MainViewModel extends StateNotifier<MainState> {
+  _MainViewModel({required final SettingsRepository repository})
+      : _repository = repository,
+        super(const MainState()) {
+    _repository.getThemeSettingStream().listen((event) {
+      if (event is theme_data.ThemeMode) {
+        state = state.copyWith(theme: event);
+      }
+    });
+  }
+
+  final SettingsRepository _repository;
+}
+
+final _mainViewModelProvider = StateNotifierProvider<_MainViewModel, MainState>(
+    (ref) => _MainViewModel(repository: GetIt.I<SettingsRepository>()));
