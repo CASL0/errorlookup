8bab5a79f713f8da8626f46041e62c2ff2b52ee8
CASL0 <gin.0046.822@gmail.com>
2023-08-20 13:47:59 +0900

    テーマ設定をUIに反映するようにmainの状態を定義

---
+++ lib/core/models/main_state.dart
@@ -0,0 +1,10 @@
+import 'package:errorlookup/core/models/theme_data.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'main_state.freezed.dart';
+
+@freezed
+class MainState with _$MainState {
+  const factory MainState({@Default(ThemeMode.system) ThemeMode theme}) =
+      _MainState;
+}
